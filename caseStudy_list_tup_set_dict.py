530a132311142b5381e3550d186a236a66a79e5f
Arifunnisa <97533510+Arifaabdul@users.noreply.github.com>
2022-11-16 19:57:29 +0530

    caseStudy_list_tup_set_dict_task

---
+++ caseStudy_list_tup_set_dict.py
@@ -0,0 +1,19 @@
+# Q1: What is the output of the following code?
+# var = "James" * 2  * 3
+
+# Q2: Create a list (month_lst) with month names and print values  as "this is month of January" 
+
+# Q3: Create a tuple (year_tup) with years from 19470 to 2022
+
+# Q3: Create a set (flowers_set) with Colors names at least 5 colors names
+
+# Q4: create a dict with your friends names and his/her quality like (funny, intelligent, smart etc)
+# ex:ex:  {"friend_name": "Thirish" , "Quality": "Smart"}
+
+# Q5: Create a dict with family members names and relations
+# ex:  {"family_member": "venkatrao" , "relation": "Father"}
+
+
+
+
+
