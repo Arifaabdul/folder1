2e96c20db21c67c1eae05ec1c4418c5bf4247133
Arifunnisa <97533510+Arifaabdul@users.noreply.github.com>
2022-11-19 12:56:19 +0530

    assinment of list tup set dict

---
+++ caseStudy_list_tup_set_dict_assignment.py
@@ -0,0 +1,42 @@
+# Q1: What is the output of the following code?
+# var = "James" * 2  * 3
+var = "James" * 2  * 3
+var
+
+
+# Q2: Create a list (month_lst) with month names and print values  as "this is month of January" 
+lst=["january","febraury","march","april","may","june","july","august","september","october","november","december"]
+for months in lst:
+    var="this is month of "+months
+    print(var)
+    
+    
+
+# Q3: Create a tuple (year_tup) with years from 19470 to 2022
+lst1=[]
+for year in range(19470,2022,-1):    
+    lst1.append(year)
+    tup=tuple(lst1)
+tup
+   
+
+# Q3: Create a set (flowers_set) with Colors names at least 5 colors names
+colours_set={"red","pink","yellow","maroon","blue"}
+colours_set
+
+
+
+# Q4: create a dict with your friends names and his/her quality like (funny, intelligent, smart etc)
+# ex:ex:  {"friend_name": "Thirish" , "Quality": "Smart"}
+dictt={"friend_name":["keerthi","padmavathi","sweety","pravallika"], "Quality":["Smart","intelligent","funny","intelligent"] }
+dictt["friend_name"]
+
+
+# Q5: Create a dict with family members names and relations
+# ex:  {"family_member": "venkatrao" , "relation": "Father"}
+dicct={"family_member": ["abdul samad","aktharunnisa","abdul anwar" ], "relation": ["Father","Mother","Brother"]}
+dicct["family_member"]
+
+
+
+
