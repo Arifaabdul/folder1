1b440c2dc0f6417fa6ed0d0f58c1f6a3cca4c6c3
Arifunnisa <97533510+Arifaabdul@users.noreply.github.com>
2022-11-23 18:36:06 +0530

    caseStudyFunctions

---
+++ caseStudy_functions_assignment.py
@@ -0,0 +1,77 @@
+
+# 1.Write a Python function to sum all the numbers in a list.
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20
+Sample_List=(8, 2, 3, 0, 7)
+def summ(Sample_List):
+    sum1=sum(Sample_List)   
+    return sum1
+summ(Sample_List)
+
+# 2. Write a function for below scenario. 
+# Param has 1000 rupees, ramu contains 5 chocolates, param give 500 rupees to ramu,
+# ramu contains 10 chocolates, param give 700 rupees to ramu, 
+# ramu contains no chocolates param give 1000 rupees to ramu.
+def param(chocolates):
+    if chocolates == 5:
+        print("param give 500 rupees to ramu")
+    elif chocolates == 10:
+        print("param give 700 rupees to ramu")    
+    elif chocolates == 0:
+        print("param give 1000 rupees to ramu")
+    else:
+        print("*********")
+param(0)
+
+# 3.Write a function for below scenario 
+# Big billon festival in amazon, bill amount on cloths more than 1000 rupees i will get 25% discount on bill.
+# bill amount on cloths less than 500 rupess i will get 5% discount on bill.
+# bill amount on cloths between 500  to 1000 rupess i will get 20% discount on bill
+def amazon_offer(amount):
+    if amount >1000:
+        offer=amount-(amount*0.25)
+        return offer
+    elif amount <500:
+        offer=amount-(amount*0.5)
+        return offer
+    elif amount >=500 or amount <=100:
+        offer=amount-(amount*0.20)
+        return offer
+    else:
+        print("no offers availible")
+amazon_offer(1000)
+   
+
+# 3.Write a function for below scenario 
+# Big billon festival in amazon, the single formal shirt value is 1500,
+# two shirts together buy(1+1 offer) shirts value is 1200
+# bulk shirts together buy(1+2 offer) shirts vlaue is 1000
+def formal_shirt(shirt):
+    if shirt==1:
+        print("shirt value is 1500")
+    elif shirt==2:
+        print("1+1 offer and shirt value is 1200")
+    elif shirt>2:
+        print("1+2 offer and shirt value is 1000")
+    else:
+        print("no offers available")
+formal_shirt(2)
+
+
+# 4.Write a Python program to reverse a string. 
+# Sample String : "ramu"
+# Expected Output : "umar"
+ramu="ramu"[::-1]
+print(ramu)
+
+
+# 5. Write a Python function that takes a list and returns a new list with unique elements of the first list. 
+# Sample List : [1,2,3,3,3,3,4,5]
+# Unique List : [1, 2, 3, 4, 5]
+sample_list=[1,2,3,3,3,3,4,5]
+def myfunction(sample_list):
+    uniq_val={1,2,3,3,3,3,4,5}
+    return uniq_val
+myfunction(sample_list)
+    
+
